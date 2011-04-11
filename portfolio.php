302d569320e1349a1c542f84110843b74799aa28
Ricardo Gonzalez <rickgc@gmail.com>
2011-04-12 00:34:18 +0100

    portfolio stab

---
+++ portfolio.php
@@ -0,0 +1,139 @@
+<!doctype html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7 ]> <html class="no-js ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]>    <html class="no-js ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]>    <html class="no-js ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+
+  <title>ryck.es - portfolio</title>
+	<meta name="description" content="ryck.me - portfolio">
+	<meta name="author" content="Ricardo Gonzalez">
+
+  <!-- Mobile viewport optimized: j.mp/bplateviewport -->
+  <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+  <!-- Place favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
+  <link rel="shortcut icon" href="/favicon.ico">
+  <link rel="apple-touch-icon-precomposed" href="apple-touch-icon-iphone3.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="apple-touch-icon-ipad.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="apple-touch-icon-iphone4.png" />
+
+
+  <!-- CSS: implied media="all" -->
+  <link rel="stylesheet" href="css/style.css?v=5">
+
+  <!-- Uncomment if you are specifically targeting less enabled mobile browsers
+  <link rel="stylesheet" media="handheld" href="css/handheld.css?v=2">  -->
+
+  <!-- All JavaScript at the bottom, except for Modernizr which enables HTML5 elements & feature detects -->
+  <script src="js/libs/modernizr-1.7.min.js"></script>
+
+</head>
+
+<body id="portfolio">
+
+<div id="wrapper">
+    <div id="wrapper-content">
+       
+        
+        <div id="header" class="clearfix">
+            <nav>
+                <ul class="menu">
+                    <li><a href="/">Home</a></li>
+                    <!--<li><a href="about">About</a><span>me &amp; myself </span></li>-->
+                    <li class="active"><a href="resume">Résumé</a><span>experience</span></li>
+                    <!--<li><a href="portfolio">Portfolio</a><span>last projects</span></li>-->
+                    <li><a href="contact">Contact</a><span>reach me</span></li>
+                </ul>
+            </nav>
+        </div>        
+    <div id="content" class="clearfix">
+    
+      <section id="portfolio-personal">
+        <hgroup>
+          <h2>Personal</h2>
+        </hgroup>
+        <hr />
+        <article>
+          <figure>
+            <a href="#" title="Screenshot"><img src="http://placekitten.com/250/200" alt="Screenshot" width="250" height="200"></a>
+            <figcaption>
+              <h3><a href="#">Description</a></h3>
+              HTML, CSS, PHP, microformats, jQuery, etc
+            </figcaption>
+          </figure>
+        </article>
+
+        <article>
+          <figure>
+            <a href="#" title="Screenshot"><img src="http://lorempixum.com/250/200" alt="Screenshot" width="250" height="200"></a>
+            <figcaption>
+              <h3><a href="#">Description</a></h3>
+              HTML, CSS, PHP, microformats, jQuery, etc
+            </figcaption>
+          </figure>
+        </article>
+        
+        <article>
+          <figure>
+            <a href="#" title="Screenshot"><img src="http://placekitten.com/250/200" alt="Screenshot" width="250" height="200"></a>
+            <figcaption>
+              <h3><a href="#">Description</a></h3>
+              HTML, CSS, PHP, microformats, jQuery, etc
+            </figcaption>
+          </figure>
+        </article>
+        
+        <article>
+          <figure>
+            <a href="#" title="Screenshot"><img src="http://lorempixum.com/250/200" alt="Screenshot" width="250" height="200"></a>
+            <figcaption>
+              <h3><a href="#">Description</a></h3>
+              HTML, CSS, PHP, microformats, jQuery, etc
+            </figcaption>
+          </figure>
+        </article>
+                        
+      </section>
+    
+    
+    </div> <!-- content -->
+        
+      <footer>
+          
+      </footer>
+         
+    </div> <!-- wrapper-content -->
+</div> <!-- wrapper -->
+
+  <!-- JavaScript at the bottom for fast page loading -->
+
+  <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if necessary -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.js"></script>
+  <script>window.jQuery || document.write("<script src='js/libs/jquery-1.5.1.min.js'>\x3C/script>")</script>
+
+
+  <!-- scripts concatenated and minified via ant build script-->
+  <script src="js/plugins.js"></script>
+  <script src="js/script.js"></script>
+  <!-- end scripts-->
+
+
+  <!--[if lt IE 7 ]>
+    <script src="js/libs/dd_belatedpng.js"></script>
+    <script>DD_belatedPNG.fix("img, .png_bg"); // Fix any <img> or .png_bg bg-images. Also, please read goo.gl/mZiyb </script>
+  <![endif]-->
+
+
+  <!-- mathiasbynens.be/notes/async-analytics-snippet Change UA-XXXXX-X to be your site's ID -->
+  <script>
+    var _gaq=[["_setAccount","UA-XXXXX-X"],["_trackPageview"]];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
+    g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
+    s.parentNode.insertBefore(g,s)}(document,"script"));
+  </script>
+
+</body>
+</html>
