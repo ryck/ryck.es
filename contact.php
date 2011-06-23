9a4c3dfd32e7c703fb8be1fc4f193d17ed1ac6c7
Ricardo Gonzalez <rickgc@gmail.com>
2011-06-23 16:43:33 +0100

    more contact love (stamp)

--- contact.php
+++ contact.php
@@ -61,25 +61,25 @@
 
       <section id="contact-form">
 
-    <ul id="errors" class="">
-        <li id="info">There were some problems with your form submission:</li>
-    </ul>
-    <p id="success">Thanks for your message! We will get back to you ASAP!</p>
-    <form method="post" action="process.php">
-        <label for="name">Name: <span class="required">*</span></label>
-        <input type="text" id="name" name="name" value="" placeholder="John Doe" required="required" autofocus="autofocus" />
+        <ul id="errors" class="">
+            <li id="info">There were some problems with your form submission:</li>
+        </ul>
+        <p id="success">Thanks for your message! We will get back to you ASAP!</p>
+        <form method="post" action="process.php">
+            <label for="name">Name: <span class="required">*</span></label>
+            <input type="text" id="name" name="name" value="" placeholder="John Doe" required="required" autofocus="autofocus" />
 
-        <label for="email">Email Address: <span class="required">*</span></label>
-        <input type="email" id="email" name="email" value="" placeholder="johndoe@example.com" required="required" />
+            <label for="email">Email Address: <span class="required">*</span></label>
+            <input type="email" id="email" name="email" value="" placeholder="johndoe@example.com" required="required" />
 
-        <label for="message">Message: <span class="required">*</span></label>
-        <textarea id="message" name="message" placeholder="Your message must be greater than 20 charcters" required="required" data-minlength="20"></textarea>
+            <label for="message">Message: <span class="required">*</span></label>
+            <textarea id="message" name="message" placeholder="Your message must be greater than 20 charcters" required="required" data-minlength="20"></textarea>
 
-        <span id="loading"></span>
-        <input type="submit" value="Holla!" id="submit-button" />
-        <p id="req-field-desc"><span class="required">*</span> indicates a required field</p>
-    </form>
-</div>
+            <span id="loading"></span>
+            <input type="submit" value="Holla!" id="submit-button" />
+            <p id="req-field-desc"><span class="required">*</span> indicates a required field</p>
+        </form>
+
 
     
       </section>
